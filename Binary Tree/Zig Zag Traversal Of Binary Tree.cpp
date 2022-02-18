75bd108a2d51ca51f7a3b0076f305ffa4c1e483a
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-02-18 12:35:41 +0530

    Update Zig Zag Traversal Of Binary Tree.cpp

--- Binary Tree/Zig Zag Traversal Of Binary Tree.cpp
+++ Binary Tree/Zig Zag Traversal Of Binary Tree.cpp
@@ -1,41 +1,54 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if(root==NULL)  return result;
-        queue<TreeNode*> q;
-        q.push(root);
-        bool lefttoright=false;
-        while(q.empty()==false){
-            int size=q.size();
-            vector<int> level;
-            for(int i=0;i<size;i++){
-                TreeNode *temp=q.front();
-                q.pop();
-                level.push_back(temp->val);
-                if(temp->left!=NULL)    q.push(temp->left);
-                if(temp->right!=NULL)   q.push(temp->right);
-            }
-            if(lefttoright){
-                reverse(level.begin(),level.end());
-                lefttoright=0;
-            }
-            else{
-                lefttoright=1;
-            }
-            result.push_back(level);
-        }
-        return result;
-    }
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+	int key;
+	Node *left,*right;
+	Node(int k){
+		key=k;
+		left=right=NULL;
+	}
 };
+vector<vector<int>> zigzag(Node *root){
+	vector<vector<int>> result;
+	if(root==NULL)
+		return result;
+	queue<Node*> q;
+	q.push(root);
+	bool lefttoright=false;
+	while(q.empty()==false){
+		int size=q.size();
+		vector<int> level;
+		for(int i=0;i<size;i++){
+			Node *temp=q.front();
+			q.pop();
+			level.push_back(temp->key);
+			if(temp->left!=NULL)
+				q.push(temp->left);
+			if(temp->right!=NULL)
+				q.push(temp->right);
+		}
+		if(lefttoright){
+			reverse(level.begin(),level.end());
+			lefttoright=0;	
+		}
+		else
+			lefttoright=1;
+		result.push_back(level);
+	}
+	return result;
+}
+int main(){
+	Node *root=new Node(3);
+	root->left=new Node(9);
+	root->right=new Node(20);
+	root->right->left=new Node(15);
+	root->right->right=new Node(7);
+	vector<vector<int>> ans;
+	ans=zigzag(root);
+	for(int i=0;i<ans.size();i++){
+		for(int j=0;j<ans[i].size();j++)
+			cout<<ans[i][j]<<" ";
+		cout<<endl;
+	}
+	return 0;
+}
