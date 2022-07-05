67635c152526a662779b64fc40cff7980c0b3a4f
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-07-05 21:01:15 +0530

    Update DFS.cpp

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+  public:
+    // Function to return a list containing the DFS traversal of the graph.
+    void dfsOfGraphRec(vector<int> adj[],int s,int V,bool *visited,vector<int> &ans){
+        visited[s]=true;
+        ans.push_back(s);
+        for(int x:adj[s]){
+            if(visited[x]==false)
+                dfsOfGraphRec(adj,x,V,visited,ans);
+        }
+    }
+    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        bool visited[V];
+        vector<int> ans;
+        for(int i=0;i<V;i++)
+            visited[i]=false;
+        for(int i=0;i<V;i++){
+            if(visited[i]==false){
+                dfsOfGraphRec(adj,i,V,visited,ans);
+            }
+        }
+        return ans;
+    }
+};
