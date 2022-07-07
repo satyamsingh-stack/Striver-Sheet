cb94263677b31a0f9e46a81528416ccd54081178
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-07-07 23:07:52 +0530

    Create Stock buy and sell.cpp

---
+++ Array/Stock buy and sell.cpp
@@ -0,0 +1,17 @@
+//Time Comp:O(N)
+//Space Comp: O(1)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0,maxi=nums[0];
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+            if(sum>maxi)
+                maxi=sum;
+            if(sum<0)
+                sum=0;
+        }
+        return maxi;
+    }
+};
