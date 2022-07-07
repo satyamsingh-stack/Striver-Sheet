8ca5a77dbbbfdc1db0dc904376f907e6935afb80
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-07-07 22:32:51 +0530

    Create Kadance's Algorithm.cpp

---
+++ Array/Kadance's Algorithm.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0,maxi=nums[0];
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+            if(sum>maxi)
+                maxi=sum;
+            if(sum<0)
+                sum=0;
+        }
+        return maxi;
+    }
+};
