9088cbf1e84efe5a59d839919ff67a7e309df074
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-07-08 21:57:18 +0530

    Create Majority Element(Ndiv2).cpp

---
+++ Array/Majority Element(Ndiv2).cpp
@@ -0,0 +1,19 @@
+// Time Comp: O(N)
+// Space Comp: O(1)
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0;
+        int el=0;
+        for(int i=0;i<nums.size();i++){
+            if(count==0)
+                el=nums[i];
+            if(nums[i]==el)
+                count++;
+            else
+                count--;
+        }
+        return el;
+    }
+};
