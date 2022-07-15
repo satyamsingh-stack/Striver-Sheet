0b99adfb957281e2525e8678b3e81570401fd517
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-07-15 19:48:24 +0530

    Create Reverse Linked List.cpp

---
+++ Linkedlist/Reverse Linked List.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *dummy=NULL;
+        while(head!=NULL){
+            ListNode *next=head->next;
+            head->next=dummy;
+            dummy=head;
+            head=next;
+        }
+        return dummy;
+    }
+};
