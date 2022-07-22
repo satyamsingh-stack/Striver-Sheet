41f21f636fb5f3997474d6c8eaf55940057b7483
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-07-22 18:35:55 +0530

    Update Lowest Common Ancestor for two given Nodes.cpp

--- Binary Tree/Lowest Common Ancestor for two given Nodes.cpp
+++ Binary Tree/Lowest Common Ancestor for two given Nodes.cpp
@@ -1,35 +1,17 @@
-#include<bits/stdc++.h>
-using namespace std;
-struct Node{
-	int key;
-	Node *left,*right;
-	Node(int k){
-		key=k;
-		left=right=NULL;
-	}
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL)
+            return root;
+        if(root->val==p->val || root->val==q->val)
+            return root;
+        TreeNode *lh=lowestCommonAncestor(root->left,p,q);
+        TreeNode *rh=lowestCommonAncestor(root->right,p,q);
+        if(lh!=NULL && rh!=NULL)
+            return root;
+        if(lh==NULL)
+            return rh;
+        else
+            return lh;
+    }
 };
-Node *LCA(Node *root,Node *p,Node *q){
-	if(root==NULL)
-		return 0;
-	if(root->key==p->key || root->key==q->key)
-		return root;
-	Node *lca1=LCA(root->left,p,q);
-	Node *lca2=LCA(root->right,p,q);
-	if(lca1!=NULL && lca2!=NULL)
-		return root;
-	if(lca1==NULL)
-		return lca2;
-	else
-		return lca1;
-}
-int main(){
-	Node *root=new Node(1);
-	root->left=new Node(2);
-	root->left->left=new Node(4);
-	root->left->right=new Node(5);
-	root->right=new Node(3);
-	root->right->left=new Node(6);
-	root->right->right=new Node(7);
-	cout<<LCA(root,root->left->left,root->left->right)->key;
-	return 0;
-}
