098dc05159c5a3b780fececa530335c376f0af86
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-08-16 11:34:23 +0530

    Create 90. Subsets II.cpp

---
+++ Recursion/90. Subsets II.cpp
@@ -0,0 +1,24 @@
+//Time Comp: O(2^n)
+//Space Comp: O(2^n * k)
+
+
+class Solution {
+public:
+    void solve(int ind,vector<int> &nums,vector<int> &ds,vector<vector<int>> &ans){
+        ans.push_back(ds);
+        for(int i=ind;i<nums.size();i++){
+            if(i!=ind && nums[i]==nums[i-1])
+                continue;
+            ds.push_back(nums[i]);
+            solve(i+1,nums,ds,ans);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        solve(0,nums,ds,ans);
+        return ans;
+    }
+};
