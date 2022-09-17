d3168c0bc9a2d93b20a21b7bfecffd93f74603fd
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-09-17 21:20:37 +0530

    Update Find Duplicate.cpp

--- Array/Find Duplicate.cpp
+++ Array/Find Duplicate.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
         }
-        int res;
-        for(auto x: mp){
-            if(x.second>=2)
-                res=x.first;
+        while(slow!=fast);
+        slow=nums[0];
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
         }
-        return res;
+        return fast;
     }
 };
