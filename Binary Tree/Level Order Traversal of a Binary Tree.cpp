c99e7118ad70c25705bdedf7e63910d56505f6d2
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2023-06-14 08:37:55 +0530

    Update Level Order Traversal of a Binary Tree.cpp

--- Binary Tree/Level Order Traversal of a Binary Tree.cpp
+++ Binary Tree/Level Order Traversal of a Binary Tree.cpp
@@ -8,34 +8,30 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ Time : O(N)
+ Space:  O(N)*/
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if(root==NULL)
-            return result;
         queue<TreeNode*> q;
+        vector<vector<int>> ans;
+        if(root==NULL)
+            return ans;
         q.push(root);
-        q.push(NULL);
-        vector<int> curr;
         while(q.empty()==false){
-            TreeNode *temp=q.front();
-            q.pop();
-            if(temp==NULL){
-                result.push_back(curr);
-                curr.resize(0);
-                if(q.size()>0)
-                    q.push(NULL);
-            }
-            else{
-                curr.push_back(temp->val);
+            int size=q.size();
+            vector<int> row;
+            for(int i=0;i<size;i++){
+                auto temp=q.front();
+                q.pop();
+                row.push_back(temp->val);
                 if(temp->left!=NULL)
                     q.push(temp->left);
                 if(temp->right!=NULL)
                     q.push(temp->right);
             }
+            ans.push_back(row);
         }
-        return result;
+        return ans;
     }
 };
